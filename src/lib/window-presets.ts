f7379cbe53ca274455110223f247692ded644d3c
mustbemo <danishqes0786@gmail.com>
2026-02-17 01:08:38 +0530

    change height on hover

--- src/lib/window-presets.ts
+++ src/lib/window-presets.ts
@@ -20,7 +20,7 @@ export const SUBSCRIBE_COLLAPSED_WINDOW_SIZE: WindowPreset = {
 
 export const SUBSCRIBE_COLLAPSED_HOVER_WINDOW_SIZE: WindowPreset = {
   width: 340,
-  height: 132,
+  height: 250,
 };
 
 export const SUBSCRIBE_EXPANDED_WINDOW_SIZE: WindowPreset = {
